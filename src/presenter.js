94e2998d8e9ec6cda215524b363921bb9731eec3
Andres Revollo <andres.revollo@ucb.edu.bo>
2025-02-17 12:15:57 -0400

    feat: Ahora se puede saludar por edad

--- src/presenter.js
+++ src/presenter.js
@@ -3,6 +3,7 @@ import saludarHora from "./saludarHora";
 
 const nombre_input = document.querySelector("#nombre");
 const genero_select = document.querySelector("#genero");
+const edad_input = document.querySelector("#edad");
 const form = document.querySelector("#saludar-form");
 const div = document.querySelector("#resultado-div");
 
@@ -11,8 +12,9 @@ form.addEventListener("submit", (event) => {
 
   const nombre = nombre_input.value;
   const genero = genero_select.value;
+  const edad = edad_input.value;
 
-  div.innerHTML = "<p> " + saludarHora() + saludar(nombre, genero) + ".</p>";
+  div.innerHTML = "<p> " + saludarHora() + saludar(nombre, genero, edad) + ".</p>";
 });
 function saludar(nombre) {
   return nombre;
